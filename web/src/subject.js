a0fdaf0fd8f5c0a2f6f240dab4daf3d0c67ac6f4
Alice Maz <alice@alicemaz.com>
2015-11-17 07:35:25 -0500

    tiny gfx tweaks

--- web/src/subject.js
+++ web/src/subject.js
@@ -12,7 +12,7 @@ const subject = {
 				subject.img.src = "ok.png";
 				//console.log(result);
 			})
-			.catch(err => subject.img.src = err === "" ? "hmm.png" : "nogood.png");
+			.catch(err => subject.img.src = err === "" ? "smile.png" : "nogood.png");
 	}, 600),
 	checkName: name => 
 		/^[a-zA-Z0-9_]{1,15}$/.test(name) ?
